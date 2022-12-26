03fb4afc5b1dfb80607650d3a96070969a587b01
ZhouTian0716 <egglord0716@gmail.com>
2022-12-26 19:29:48 +1100

    before footing

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,22 @@
-console.log("hi")
+const navigation = document.querySelector('#navigation');
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+	const currentScroll = window.pageYOffset;
+	if (currentScroll <= 0) {
+		navigation.classList.remove("scroll-up");
+		return;
+	}
+
+	if (currentScroll > lastScroll && !navigation.classList.contains("scroll-down")) {
+		navigation.classList.remove("scroll-up");
+		navigation.classList.add("scroll-down");
+	} else if (
+		currentScroll < lastScroll &&
+		navigation.classList.contains("scroll-down")
+	) {
+		navigation.classList.remove("scroll-down");
+		navigation.classList.add("scroll-up");
+	}
+	lastScroll = currentScroll;
+});
